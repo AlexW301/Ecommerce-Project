a32dd2d6d266f1c9ea220d3212a9fb01dfa1da7c
Alex Waller <alex@pmlhomeloans.com>
2022-05-03 12:59:00 -0400

    update
    stopped at 1:13:56 in the video

--- ecommerce/components/NavBar.jsx
+++ ecommerce/components/NavBar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShopping, AIOutlineShopping } from 'react-icons/ai'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='navbar-container'>
+      <p>
+        <Link href={'/'}>
+          JSM Headphones
+        </Link>
+      </p>
+
+      <button type="button" className='cart-icon' onClick={''}>
+          <AiOutlineShopping />
+          <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -5,18 +5,18 @@ import { client } from '../lib/client';
 const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
-
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+        {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
-      <FooterBanner/>
+      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </>
   );
 };
