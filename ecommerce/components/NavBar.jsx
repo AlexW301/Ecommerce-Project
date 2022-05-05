b5d98b232bc4fab0dddb344ced1f0d6b7aa2e9e7
Alex Waller <alex@pmlhomeloans.com>
2022-05-05 08:10:07 -0400

    update
    paused video at 2:16:34

--- ecommerce/components/NavBar.jsx
+++ ecommerce/components/NavBar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping, AIOutlineShopping } from 'react-icons/ai'
 
+import { Cart } from './'
+import { useStateContext } from '../context/StateContext'
+
 function NavBar() {
+  const { showCart, setShowCart, totalQuantities } = useStateContext()
   return (
     <div className='navbar-container'>
       <p>
@@ -11,10 +15,11 @@ function NavBar() {
         </Link>
       </p>
 
-      <button type="button" className='cart-icon' onClick={''}>
+      <button type="button" className='cart-icon' onClick={() => setShowCart(true)}>
           <AiOutlineShopping />
-          <span className='cart-item-qty'>1</span>
+          <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+      {showCart && <Cart />}
     </div>
   )
 }
