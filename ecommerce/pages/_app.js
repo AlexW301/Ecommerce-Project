0abe86178d2d9cc7459b6434bbef5ac649fd7941
Alex Waller <alex@pmlhomeloans.com>
2022-05-17 12:26:42 -0400

    Update _app.js

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -4,6 +4,7 @@ import { Layout } from "../components";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from 'react-hot-toast'
 
+
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
